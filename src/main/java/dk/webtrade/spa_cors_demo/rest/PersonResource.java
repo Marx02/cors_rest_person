caba559b654d6fd8d02321939b700f5560ecb954
Marx02 <casper_marx@msn.com>
2019-03-13 15:18:37 +0100

    BIG COMMIT, REST PLS WORK

--- src/main/java/dk/webtrade/spa_cors_demo/rest/PersonResource.java
+++ src/main/java/dk/webtrade/spa_cors_demo/rest/PersonResource.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dk.webtrade.spa_cors_demo.rest;
 
 import com.google.gson.Gson;
@@ -11,6 +7,7 @@ import dk.webtrade.spa_cors_demo.entity.Person;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -19,13 +16,10 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-/**
- * REST Web Service
- *
- * @author thomas
- */
+
 @Path("person")
 public class PersonResource {
+
     DataFacade df = new DataFacade();
     Gson gson = new Gson();
     @Context
@@ -38,7 +32,9 @@ public class PersonResource {
     }
 
     /**
-     * Retrieves representation of an instance of dk.webtrade.spa_cors_demo.PersonResource
+     * Retrieves representation of an instance of
+     * dk.webtrade.spa_cors_demo.PersonResource
+     *
      * @return an instance of java.lang.String
      */
 //    @GET
@@ -52,13 +48,13 @@ public class PersonResource {
     public Response getPersons() {
         //TODO return proper representation object
         return Response.ok()
-                .header("Access-Control-Allow-Origin", "*")
-                .header("Access-Control-Allow-Credentials", "true")
-                .header("Access-Control-Allow-Headers","origin, content-type, accept, authorization")
-                .header("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                //                .header("Access-Control-Allow-Origin", "*")
+                //                .header("Access-Control-Allow-Credentials", "true")
+                //                .header("Access-Control-Allow-Headers","origin, content-type, accept, authorization")
+                //                .header("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS, HEAD")
                 .entity(gson.toJson(df.getAllPersons())).build();
     }
-    
+
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
@@ -67,14 +63,32 @@ public class PersonResource {
         return Response.ok().entity(gson.toJson(df.getPerson(id))).build();
     }
 
+    @GET
+    @Path("/allasarray")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllPerson(@PathParam("id") int id) {
+        //TODO return proper representation object
+        return Response.ok().entity(gson.toJson(df.getAllPersons().values())).build();
+    }
+
     /**
      * PUT method for updating or creating an instance of PersonResource
+     *
      * @param content representation for the resource
      */
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content) {
+    public void postPerson(String content) {
         Person p = gson.fromJson(content, Person.class);
         df.addPerson(p);
     }
+
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteCustomer(@PathParam("id") int id) {
+        Person p = df.removePerson(id);
+        return Response.ok().entity(gson.toJson(p)).build();
+    }
+
 }
